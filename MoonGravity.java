5dbb18df46ee494ddeac2e2419f276cc9c8b5ae0
Kent Collins <kent.r.collins@gmail.com>
2014-10-16 20:24:32 +0100

    Create MoonGravity.java

---
+++ MoonGravity.java
@@ -0,0 +1,17 @@
+/**
+* 
+* Determines the acceleration due to gravity at some distance
+* from the center of the moon.  The distance is read as input
+* from the user.
+*
+* @author <your name>
+* @version <date of completion>
+*/
+
+public class MoonGravity {
+  
+  public static void main(String[] args) {
+    //TODO - replace this comment with working code
+  }
+  
+}
