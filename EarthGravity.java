41326ca5678ec4f6f68cef617015bc91ff71dcd5
Kent Collins <kent.r.collins@gmail.com>
2014-10-16 20:23:53 +0100

    Create EarthGravity.java

---
+++ EarthGravity.java
@@ -0,0 +1,17 @@
+/**
+* 
+* Determines the acceleration due to gravity at some distance
+* from the center of the earth.  The distance is read as input
+* from the user.
+*
+* @author <your name>
+* @version <date of completion>
+*/
+
+public class EarthGravity {
+  
+  public static void main(String[] args) {
+    //TODO - replace this comment with working code
+  }
+  
+}
