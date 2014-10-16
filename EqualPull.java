9340d6eebd26bc28ec9b99ad135e3c7e848dcc46
Kent Collins <kent.r.collins@gmail.com>
2014-10-16 20:26:30 +0100

    Create EqualPull.java

---
+++ EqualPull.java
@@ -0,0 +1,19 @@
+/**
+* 
+* Determines the distance at which the pulling force between two
+* bodies is exactly equal.  
+*
+* Takes as input the mass of one body, the mass of the other, and
+* the distance between the two bodies.
+*
+* @author <your name>
+* @version <date of completion>
+*/
+
+public class EqualPull {
+  
+  public static void main(String[] args) {
+    //TODO - replace this comment with working code
+  }
+  
+}
